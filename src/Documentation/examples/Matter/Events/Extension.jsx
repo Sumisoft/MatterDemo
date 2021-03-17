057286aa4ba9a3822f325cac7fdce35e9f4b4e7a
Nik <nsumikawa@gmail.com>
2021-03-16 21:16:52 -0400

    partial refacotr

---
+++ src/Documentation/examples/Matter/Events/Extension.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import Matter, {Bodies} from "matter-js";
+
+import devSpace from '../testComponents/devSpace'
+
+class Scene extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    var Engine = Matter.Engine,
+      Render = Matter.Render,
+      World = Matter.World,
+      Body = Matter.Body,
+      Bodies = Matter.Bodies,
+      Mouse = Matter.Mouse,
+      Events = Matter.Events,
+      Composite = Matter.Composite,
+      Composites = Matter.Composites,
+      MouseConstraint = Matter.MouseConstraint;
+
+
+    var matterComp = devSpace(this.refs.scene)
+
+    var render = matterComp.render
+    var engine = matterComp.engine
+
+
+    var rectA = Bodies.rectangle(300, 200, 50, 50, {
+      restitution: 0.5,
+      isStatic: false,
+      inertia: Infinity, });
+
+    Matter.Body.set(rectA, 'health', 2)
+    World.add(engine.world, [rectA]);
+
+
+
+
+    // add mouse control
+    var mouse = Mouse.create(render.canvas),
+      mouseConstraint = MouseConstraint.create(engine, {
+        mouse: mouse,
+        constraint: {
+          stiffness: 0.2,
+          render: {
+            visible: false
+          }
+        }
+      });
+
+    World.add(engine.world, mouseConstraint);
+
+    Matter.Events.on(mouseConstraint, "mousedown", function(event) {
+      // console.log(event.source.body)
+      // dragBody = event.source.body;
+
+      console.log( event.source.body )
+    });
+
+
+    Engine.run(engine);
+
+    Render.run(render);
+  }
+
+  render() {
+     return <div ref="scene" />;
+   }
+ }
+ export default Scene;
--- src/Game/components/Characters/matterObj.jsx
+++ src/Game/components/Characters/matterObj.jsx
@@ -5,47 +5,53 @@ import Matter from "matter-js";
 
 class matterObj{
 
-  constructor( health ){
-    this.body = undefined
-    this.health = health
+  constructor( props ){
+    this.bodyObj = undefined
+
+    this.health = props.health
+    this.group = props.group
+    this.type = props.type
   }
 
-  rectangle( x, y, h, w, params={} ){
-    this.body = Matter.Bodies.rectangle(x, y, h, w, params);
+
+  get health(){ return this._health }
+  set health(value){ this._health = value}
+
+  get type(){ return this._type }
+  set type(value){ this._type = value}
+
+  // push key attributes into the body object
+  setParameters(){
+    Matter.Body.Set(this.bodyObj, 'health', this.value )
+    Matter.Body.Set(this.bodyObj, 'group', this.group )
+    Matter.Body.Set(this.bodyObj, 'type', this.type )
   }
 
-  circle( x, y, r, params={} ){
-    this.body = Matter.Bodies.circle(x, y, r, params);
+  character( x, y, h, w, params={} ){
+    const params = this.getParams(this.group)
+    this.bodyObj = Matter.Bodies.rectangle(x, y, h, w, params);
+    this.setParameters()
+  }
+
+  projectile(x, y, r, params={}){
+    const params = this.getParams(this.group)
+    this.bodyObj = Matter.Bodies.circle(x, y, r, params);
+    this.setParameters()
+    this.setFriction()
   }
 
   setFriction(){
-    this.body.friction = 0.05;
-    this.body.frictionAir = 0.0005;
-    this.body.restitution = 0.9;
+    this.bodyObj.friction = 0.05;
+    this.bodyObj.frictionAir = 0.0005;
+    this.bodyObj.restitution = 0.9;
   }
 
   setVelocity( x, y ){
-    Matter.Body.setVelocity( this.body, {x: x, y: y})
+    Matter.Body.setVelocity( this.bodyObj, {x: x, y: y})
   }
 
-  isCollision( bodyA, bodyB){
-    if((bodyA === this.body)|(bodyB === this.body)) return true
-    return false
-  }
 
-  collision( engine ){
-
-    this.health = this.health - 1
-
-    if(this.health === 0){
-      Matter.World.remove(engine.world, this.body);
-      return true
-    }
-
-    return false
-  }
-
-  getParams( category ){
+  getParams( group ){
     const style = {
       0: {
         id: 0x0002,
@@ -63,16 +69,18 @@ class matterObj{
 
     return {
       collisionFilter: {
-          category: style[category].category,
-          mask: style[category].mask
+          category: style[group].category,
+          mask: style[group].mask
       },
       render: {
-          strokeStyle: style[category].color,
+          strokeStyle: style[group].color,
           fillStyle: 'transparent',
           lineWidth: 2
       }
     }
   }
+
+
 
 }
 
